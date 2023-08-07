4f3c5b90fa54f948cfacaf2a2ffa9cac9828687a
Beto Mossmann <gilbertomossmann@gmail.com>
2023-08-07 17:10:45 -0300

    add common components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
 import '../globals.css'
+import { Bottombar, LeftSidebar, RightSidebar, Topbar } from '@/components'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Bottombar } from './shared/bottombar'
+export { default as Topbar } from './shared/topbar'
+export { default as RightSidebar } from './shared/right-sidebar'
+export { default as LeftSidebar } from './shared/left-sidebar'
