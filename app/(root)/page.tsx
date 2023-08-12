b42ef5098db1bab0d92032447631b687b8244594
Beto Mossmann <gilbertomossmann@gmail.com>
2023-08-12 09:09:45 -0300

    add prettier config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,25 @@
-import ThreadCard from '@/components/cards/thread-card'
+import ThreadCard from "@/components/cards/thread-card";
 import { fetchPosts } from '@/lib/actions/thread.actions'
 
-import { currentUser } from '@clerk/nextjs'
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30)
-  const user = await currentUser()
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
 
   return (
     <>
-      <h1 className='head-text text-left'>Home</h1>
-
-      <section className='mt-9 flex flex-col gap-10'>
+      <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
-          <p className='no-result'>No threads found</p>
+          <p className="no-result">No threads found</p>
         ) : (
           <>
             {result.posts.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id || ''}
+                currentUserId={user?.id || ""}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
@@ -33,5 +32,5 @@ export default async function Home() {
         )}
       </section>
     </>
-  )
+  );
 }
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  tabWidth: 2,
+  endOfLine: "lf",
+  trailingComma: "none",
+  singleQuote: true,
+};
+
+export default config;
