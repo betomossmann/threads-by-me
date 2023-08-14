fb58fb04998c1f72a77562941af0ab35b129289e
Beto Mossmann <gilbertomossmann@gmail.com>
2023-08-14 12:45:13 -0300

    ignore ts errors

--- next.config.js
+++ next.config.js
@@ -1,35 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   experimental: {
     serverActions: true,
-    serverComponentsExternalPackages: ["mongoose"],
+    serverComponentsExternalPackages: ['mongoose']
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.clerk.com",
+        protocol: 'https',
+        hostname: 'img.clerk.com'
       },
       {
-        protocol: "https",
-        hostname: "images.clerk.dev",
+        protocol: 'https',
+        hostname: 'images.clerk.dev'
       },
       {
-        protocol: "https",
-        hostname: "uploadthing.com",
+        protocol: 'https',
+        hostname: 'uploadthing.com'
       },
       {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
+        protocol: 'https',
+        hostname: 'placehold.co'
+      }
     ],
     typescript: {
-      ignoreBuildErrors: true,
-    },
-  },
-};
+      ignoreBuildErrors: true
+    }
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
